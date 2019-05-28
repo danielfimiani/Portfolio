9fc04f6ba6d5e712c5c7cb28f03a3b6b6689a199
Daniel Fimiani <dfimiani@sistran.com.ar>
2019-05-28 17:45:55 -0300

    Varios nav-bar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-M.AutoInit();
+$(document).ready(function () {
+    $(window).on('scroll',function() {
+        if ($(window).scrollTop()) {
+            $('nav').addClass('sticky-nav');
+            $('nav>div>ul>li>a').addClass('white-text');
+            $('h1').css("color","white");
+        }else{
+            $('nav').removeClass('sticky-nav');
+            $('nav>div>ul>li>a').removeClass('white-text')
+            $('h1').removeClass('white-text');
+            $('h1').css("color","black");
+        }
+    });
+});
